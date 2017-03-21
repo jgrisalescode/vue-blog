e866f16e0e1702ab0fd356ceb6de29e9da0c9388
Jorge García <jorge@aprendible.com>
2017-03-21 09:41:45 -0500

    Lesson 11

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Laravel',
+    'name' => 'Agencia de la Web',
 
     /*
     |--------------------------------------------------------------------------
@@ -77,7 +77,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'es',
 
     /*
     |--------------------------------------------------------------------------
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,35 @@
 <?php
 
-Route::get('/', function () {
-	$posts = App\Post::latest('published_at')->get();
+Route::get('/', 'PagesController@home');
 
-    return view('welcome', compact('posts'));
+Route::get('home', 'HomeController@index');
+
+Route::group([
+	'prefix' => 'admin', 
+	'namespace' => 'Admin', 
+	'middleware' => 'auth'], 
+function(){
+	Route::get('posts', 'PostsController@index');
+	// rutas de administracion
 });
 
-Route::get('admin', function(){
-	return view('admin.home');
-});
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
-Route::get('posts', function(){
-	return App\Post::all();
-});
+// Registration Routes...
+// Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+// Route::post('register', 'Auth\RegisterController@register');
 
-Route::get('/home', 'HomeController@index');
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+
+
+
+
+
