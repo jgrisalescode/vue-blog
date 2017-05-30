d2ebf161aa13c7f14fc3bfce31ecd9fabdcf0719
Jorge García <jorge@aprendible.com>
2017-05-29 23:57:46 -0500

    21 - Urls amigables

--- app/Http/Controllers/Admin/PostsController.php
+++ app/Http/Controllers/Admin/PostsController.php
@@ -38,6 +38,7 @@ class PostsController extends Controller
         // return Post::create($request->all());
         $post = new Post;
         $post->title = $request->get('title');
+        $post->url = str_slug($request->get('title'));
         $post->body = $request->get('body');
         $post->excerpt = $request->get('excerpt');
         $post->published_at = $request->has('published_at') ? Carbon::parse($request->get('published_at')) : null;
