d05868698acfa4de5626415e517d12b1ca91c5d5
Jorge García <jorge@aprendible.com>
2017-10-15 15:04:44 -0500

    49 - Vistas polimórficas

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,27 +2,15 @@
 
 @section('content')
 <article class="post container">
-    @if ($post->photos->count() === 1)
-        <figure><img src="{{ url($post->photos->first()->url) }}" class="img-responsive" alt=""></figure>
-    @elseif($post->photos->count() > 1)
-        @include('posts.carousel')
-    @elseif($post->iframe)
-        <div class="video">
-            {!! $post->iframe !!}
-        </div>
-    @endif
+
+    @include( $post->viewType() )
+
     <div class="content-post">
-        <header class="container-flex space-between">
-            <div class="date">
-                <span class="c-gris">{{ optional($post->published_at)->format('M d') }}</span>
-            </div>
-            @if ($post->category)
-                <div class="post-category">
-                    <span class="category">{{ $post->category->name }}</span>
-                </div>
-            @endif
-        </header>
+
+        @include('posts.header')
+
         <h1>{{ $post->title }}</h1>
+
         <div class="divider"></div>
 
         <div class="image-w-text">
@@ -31,12 +19,10 @@
 
         <footer class="container-flex space-between">
             @include('partials.social-links', ['description' => $post->title])
-            <div class="tags container-flex">
-                @foreach($post->tags as $tag)
-                <span class="tag c-gris">#{{ $tag->name }}</span>
-                @endforeach
-            </div>
+
+            @include('posts.tags')
         </footer>
+
         <div class="comments">
             <div class="divider"></div>
             <div id="disqus_thread"></div>
