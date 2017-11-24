a67810fca2409624ca4c37c2ffdf9171c1ae2bf0
Jorge García <jorge@aprendible.com>
2017-11-24 14:36:56 -0500

    62 - Crear usuarios - Parte 1

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -54,16 +54,9 @@
                 <div class="box-body">
                     <form method="POST" action="{{ route('admin.users.roles.update', $user) }}">
                         {{ csrf_field() }} {{ method_field('PUT') }}
-                        @foreach ($roles as $role)
-                            <div class="checkbox">
-                                <label>
-                                    <input name="roles[]" type="checkbox" value="{{ $role->name }}"
-                                        {{ $user->roles->contains($role->id) ? 'checked':'' }}>
-                                    {{ $role->name }} <br>
-                                    <small class="text-muted">{{ $role->permissions->pluck('name')->implode(', ') }}</small>
-                                </label>
-                            </div>
-                        @endforeach
+
+                        @include('admin.roles.checkboxes')
+
                         <button class="btn btn-primary btn-block">Actualizar roles</button>
                     </form>
                 </div>
@@ -75,15 +68,9 @@
                 <div class="box-body">
                     <form method="POST" action="{{ route('admin.users.permissions.update', $user) }}">
                         {{ csrf_field() }} {{ method_field('PUT') }}
-                        @foreach ($permissions as $id => $name)
-                            <div class="checkbox">
-                                <label>
-                                    <input name="permissions[]" type="checkbox" value="{{ $name }}"
-                                        {{ $user->permissions->contains($id) ? 'checked':'' }}>
-                                    {{ $name }}
-                                </label>
-                            </div>
-                        @endforeach
+
+                        @include('admin.permissions.checkboxes')
+
                         <button class="btn btn-primary btn-block">Actualizar permisos</button>
                     </form>
                 </div>
