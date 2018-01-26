8d74fbe778fb3f72b6fba0eef8bb85c651793f6b
Jorge García <jorge@aprendible.com>
2018-01-25 20:35:19 -0500

    78 - Implementando la página de archivo

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,7 +5,7 @@
 @if (isset($title))
     <h3>{{ $title }}</h3>
 @endif
-@foreach($posts as $post)
+@forelse($posts as $post)
     <article class="post">
 
         @include( $post->viewType('home') )
@@ -31,9 +31,17 @@
             </footer>
         </div>
     </article>
-@endforeach
+@empty
+    <article class="post">
+        <div class="content-post">
+
+            <h1>No hay publicaciones todavía.</h1>
+
+        </div>
+    </article>
+@endforelse
 </section><!-- fin del section.posts -->
 
-{{ $posts->links() }}
+{{ $posts->appends(request()->all())->links() }}
 
 @stop
