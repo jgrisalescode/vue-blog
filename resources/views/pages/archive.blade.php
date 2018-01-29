32808775028e7adf71cde1a0f73ed3502c6012d8
Jorge García <jorge@aprendible.com>
2018-01-29 16:14:52 -0500

    79 - Agrupar posts por mes y año

--- resources/views/pages/archive.blade.php
+++ resources/views/pages/archive.blade.php
@@ -33,9 +33,9 @@
 
                 <h3 class="text-capitalize">posts by month</h3>
                 <ul class="list-unstyled">
-                    <li>August 2015</li>
-                    <li>September 2015</li>
-                    <li>October 2015</li>
+                    @foreach ($archive as $date)
+                        <li>{{ $date->month }} {{ $date->year }} ({{ $date->posts }})</li>
+                    @endforeach
                 </ul>
             </div>
         </div>
